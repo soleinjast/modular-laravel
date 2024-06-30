cf2cfe5e8a24246eadf6a589698b5b85a314b433
YektaRoustaei <150620345+YektaRoustaei@users.noreply.github.com>
2024-06-30 21:14:09 +0100

    new added

--- modules/Order/Checkout/OrderReceived.php
+++ modules/Order/Checkout/OrderReceived.php
@@ -18,8 +18,7 @@ class OrderReceived extends Mailable
      * Create a new message instance.
      */
     public function __construct(
-        public 
-        OrderDto $order
+        public OrderDto $order
 
 
     ) {
